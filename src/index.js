7d8bd58cb063bd0317192e6e8959f32630ed19a9
duy1111 <maixuanduy0605@gmail.com>
2022-10-22 22:36:15 +0700

    [MVC] Routes & Controllers

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ const morgan = require('morgan');
 const path = require('path');
 const app = express();
 const port = 3000;
-
+const route = require('./routes/index')
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.urlencoded({
+  extended:true
+}));
+app.use(express.json());
 // HTTP logger
 app.use(morgan('combined'));
 
@@ -16,13 +19,11 @@ app.engine('hbs', engine({extname: '.hbs'}));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
+
+//Route init
+route(app);
 // method
-app.get('/', (req, res) => {
-  res.render('home');
-})
-app.get('/news', (req, res) => {
-  res.render('news');
-})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
