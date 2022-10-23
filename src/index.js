651f1219ee47617332455fdd58f642978afb5235
duy1111 <maixuanduy0605@gmail.com>
2022-10-24 00:41:26 +0700

    Prettier

--- src/index.js
+++ src/index.js
@@ -1,30 +1,29 @@
 const express = require('express');
-const {engine} = require('express-handlebars')
+const { engine } = require('express-handlebars');
 const morgan = require('morgan');
 const path = require('path');
 const app = express();
 const port = 3000;
-const route = require('./routes/index')
+const route = require('./routes/index');
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({
-  extended:true
-}));
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
 app.use(express.json());
 // HTTP logger
 app.use(morgan('combined'));
 
-
 // Template engine
-app.engine('hbs', engine({extname: '.hbs'}));
+app.engine('hbs', engine({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
-
 
 //Route init
 route(app);
 // method
 
-
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
